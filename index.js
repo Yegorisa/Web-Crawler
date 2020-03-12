8a1be98a099a53e16b96448f3ca4b34e06850412
egor.egorov <egor.egorov@xero.com>
2020-03-12 22:42:17 +1300

    Added UI and completed logic

--- index.js
+++ index.js
@@ -13,16 +13,21 @@ server.use(express.static('public'));
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8080;
+    port = 8080;
 }
 server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.post('/clicked', (req, res) => {
-  console.log("RECIEVED REQUEST HEU HEY")
-  startCrawling(recordInfo, res);
-
+server.get('/clicked', (req, res) => {
+    console.log("RECIEVED REQUEST HEU HEY");
+    count = 0;
+    ids = [];
+    oldProfiles = [];
+    clientProfiles = [];
+    newProfiles = [];
+    megaResponse = null;
+    startCrawling(recordInfo, res);
 });
 
 server.listen(port);
@@ -30,7 +35,7 @@ server.listen(port);
 
 var ids = [];
 
-var oldEmails = [];
+var oldProfiles = [];
 var clientProfiles = [];
 var newProfiles = [];
 
@@ -42,130 +47,129 @@ var url = process.env.MONGODB_URI || 'mongodb://heroku_45jq6d6k:geeti0e5hag4vt1i
 const dbName = "heroku_45jq6d6k";
 
 var c = new Crawler({
-  maxConnections: 10,
+    maxConnections: 10,
 });
 
-function startCrawling(recordIds, response) {
-  megaResponse = response
-
-  // var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb';
-
+c.on('drain', function () {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db(dbName);
-    dbo.collection("customers").find({}).toArray(function (err, result) {
+
+    //deleteAllPreviousRecords(dbo);
+
+    dbo.collection("customers").insertMany(clientProfiles, function (err, res) {
       if (err) throw err;
-      oldEmails = result.map((profile) => (profile.email));
-      console.log('---OLD PROFILES---');
-      console.log(oldEmails.length);
+      console.log("Number of documents inserted: " + res.insertedCount);
     });
+    console.log('---ALL PROFILES---');
+    console.log(clientProfiles.length);
+
+    const oldEmails = oldProfiles.map(p => p.email);
+    newProfiles = clientProfiles.filter(clientInfo => !oldEmails.includes(clientInfo.email));
+    console.log('---NEW PROFILES---');
+    console.log(newProfiles.length);
 
     db.close();
+    megaResponse.send({oldProfiles, newProfiles});
+    // megaResponse.sendStatus(201);
   });
-  getIds(recordInfo, response)
+});
+
+function startCrawling(recordIds, response) {
+    megaResponse = response;
+
+    // var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb';
+
+    try {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db(dbName);
+            dbo.collection("customers").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                oldProfiles = result;
+                console.log('---OLD PROFILES---');
+                console.log(oldProfiles.length);
+                db.close();
+            });
+        });
+        getIds(recordInfo, response)
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
-function getIds(recordIds, response) {
+function getIds(recordIds) {
+    c.queue([{
 
+        uri: 'https://lbp.ewr.govt.nz/PublicRegister/Search.aspx?t=Auckland&lc=LIC002&r=Auckland&search=1&p=' + ++count + '&sc=1',
 
-  c.queue([{
+        // The global callback won't be called
+        callback: function (error, res, done) {
+            var $ = res.$;
+            var noResultBox = $('.messageBox h2').toString();
 
-    uri: 'https://lbp.ewr.govt.nz/PublicRegister/Search.aspx?t=Auckland&lc=LIC002&r=Auckland&search=1&p=' + ++count + '&sc=1',
+            if (error) {
+                console.log('error ' + error);
+                done();
+            // } else if (count === 2) {
+            //     console.log('length ' + ids.length);
+            //     recordIds();
+            //     done();
+            } else if (noResultBox.includes('No results found')) {
+                console.log('length ' + ids.length);
+                recordIds();
+                done();
+            } else {
+                console.log(count);
 
-    // The global callback won't be called
-    callback: function (error, res, done) {
-      var $ = res.$;
-      var noResultBox = $('.messageBox h2').toString();
+                $('.striped tr').each(function (i, element) {
 
-      if (error) {
-        console.log('error ' + error);
-        done();
-      }
-
-      else if (count === 2) {
-        console.log('length ' + ids.length);
-        recordIds();
-        done();
-      }
-
-      else if (noResultBox.includes('No results found')) {
-        console.log('length ' + ids.length);
-        recordIds();
-        done();
-      }
-
-      else {
-        console.log(count);
-
-        $('.striped tr').each(function (i, element) {
-
-          var el = $(element).find('a').attr('href');
-          var id = el.substring(el.indexOf(search) + search.length);
-          if (id.includes('BP')) {
-            ids.push(el);
-          }
-        });
-        getIds(recordIds);
-        done();
-      }
-    }
-  }]);
+                    var el = $(element).find('a').attr('href');
+                    var id = el.substring(el.indexOf(search) + search.length);
+                    if (id.includes('BP')) {
+                        ids.push(el);
+                    }
+                });
+                getIds(recordIds);
+                done();
+            }
+        }
+    }]);
 }
 
 function deleteAllPreviousRecords(dbo) {
-  dbo.collection("customers").deleteMany(function (err, obj) {
-    if (err) throw err;
-    console.log(obj.result.n + " document(s) deleted");
-  });
+    dbo.collection("customers").deleteMany(function (err, obj) {
+        if (err) throw err;
+        console.log(obj.result.n + " document(s) deleted");
+    });
 }
 
 function recordInfo() {
-  c.on('drain', function () {
-    MongoClient.connect(url, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db(dbName);
 
-      deleteAllPreviousRecords(dbo);
+    ids.forEach(function (uri) {
+        console.log('uri ' + uri);
+        c.queue([{
+            uri: 'https://lbp.ewr.govt.nz' + uri,
 
-      dbo.collection("customers").insertMany(clientProfiles, function (err, res) {
-        if (err) throw err;
-        console.log("Number of documents inserted: " + res.insertedCount);
-      });
-      console.log('---ALL PROFILES---');
-      console.log(clientProfiles.length);
+            // The global callback won't be called
+            callback: function (error, res, done) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var $ = res.$;
 
-      newProfiles = clientProfiles.filter(clientInfo => !oldEmails.includes(clientInfo.email));
-      console.log('---NEW PROFILES---');
-      console.log(newProfiles.length);
+                    const clientInfo = {
+                        name: $('.formContainer > h2').text().split(' -')[0],
+                        phone: $('#ctl00_MainContent_ucLbpDetails_fkPhoneNumber_View').text(),
+                        email: $('#ctl00_MainContent_ucLbpDetails_fkEmailAddress_View').text()
+                    };
 
-      db.close();
-      megaResponse.send(clientProfiles)
-      megaResponse.sendStatus(201);
-    });
-  });
-  ids.forEach(function (uri) {
-    console.log('uri ' + uri);
-    c.queue([{
-      uri: 'https://lbp.ewr.govt.nz' + uri,
-
-      // The global callback won't be called
-      callback: function (error, res, done) {
-        if (error) {
-          console.log(error);
-        } else {
-          var $ = res.$;
-
-          const clientInfo = {
-            name: $('.formContainer > h2').text().split(' -')[0],
-            phone: $('#ctl00_MainContent_ucLbpDetails_fkPhoneNumber_View').text(),
-            email: $('#ctl00_MainContent_ucLbpDetails_fkEmailAddress_View').text()
-          }
-
-          clientProfiles.push(clientInfo);
-        }
-        //console.log('add a record ' + ++recordCounter);
-        done();
-      }
-    }]);
-  })
+                    clientProfiles.push(clientInfo);
+                }
+                //console.log('add a record ' + ++recordCounter);
+                done();
+            }
+        }]);
+    })
 }
--- public/client.js
+++ public/client.js
@@ -1,17 +1,34 @@
 const button = document.getElementById('myButton');
 button.addEventListener('click', function (e) {
-  console.log('button was clicked');
-  fetch('/clicked', { method: 'POST' })
-    .then(function (response) {
-      if (response.ok) {
-        response.body
-        console.log('DATA IS ' + response.body)
-        console.log('Click was recorded');
-        return;
-      }
-      throw new Error('Request failed.');
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    console.log('button was clicked');
+    document.getElementById("myTable").innerHTML = "";
+    document.getElementById("myTable2").innerHTML = "";
+    const button = document.getElementById("myButton");
+    button.disabled = true;
+    fetch('/clicked', {method: 'GET'})
+        .then(async function (response) {
+            button.disabled = false;
+            if (response.ok) {
+                const responseJson = await response.json();
+                console.log('DATA IS ' + JSON.stringify(responseJson));
+                console.log('Click was recorded');
+                responseJson.oldProfiles.forEach(profile => addRow(profile, "myTable"));
+                responseJson.newProfiles.forEach(profile => addRow(profile, "myTable2"));
+                return;
+            }
+            throw new Error('Request failed.');
+        })
+        .catch(function (error) {
+            button.disabled = false;
+            console.log(error);
+        });
 });
+
+function addRow(obj, tableName) {
+    var table = document.getElementById(tableName);
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = obj.name;
+    cell2.innerHTML = obj.email;
+}
